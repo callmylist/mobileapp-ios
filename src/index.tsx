e24365ee083845e033358b743e50382694728fbf
Wang Dan <apaladin1993@outlook.com>
2020-05-08 11:10:36 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,54 @@ import {
     createSwitchNavigator,
     SafeAreaView
 } from 'react-navigation';
+import {
+  createDrawerNavigator
+} from 'react-navigation-drawer';
 import React, { Component } from 'react';
 import WelcomeScreen from './screens/welcome'
+import LoginScreen from './screens/login'
+import RegisterScreen from './screens/register'
+import DashboardScreen from './screens/dashboard'
+import MessageCenterScreen from './screens/messagecenter'
+import Menu from './components/menu'
 import { View, StatusBar, Platform } from 'react-native';
+
+const Dashboard = createDrawerNavigator({
+  Dashboard: {
+    screen: DashboardScreen
+  },
+  MessageCenter: {
+    screen: MessageCenterScreen
+  }
+}, {
+  initialRouteName: 'Dashboard',
+  contentComponent: Menu,
+  drawerPosition: 'left',
+  drawerWidth: 300,
+  headerMode: 'none',
+  drawerType: 'front',
+  overlayColor: 'transparent',
+  backBehavior: 'none',
+  drawerBackgroundColor: '#0000FF00',
+});
+
+const AuthNavigator = createSwitchNavigator(
+  {
+    LoginScreen,
+    RegisterScreen,
+  },
+  {
+    initialRouteName: 'LoginScreen',
+    headerMode: 'none',
+    navigationOptions: { header: null },
+  },
+);
 
 const MainNavigator = createSwitchNavigator(
     {
       WelcomeScreen,
+      AuthNavigator,
+      Dashboard
     },
     {
       initialRouteName: 'WelcomeScreen',
@@ -26,15 +67,7 @@ export default class MainApp extends Component {
   
     render() {
       return (
-        <SafeAreaView style={{ flex: 1 }}>
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent={true}
-          />
-  
-          <AppNav style={{ flex: 1 }} />
-        </SafeAreaView>
+        <AppNav style={{ flex: 1 }} />
       );
     }
   }
--- src/screens/welcome.tsx
+++ src/screens/welcome.tsx
@@ -1,12 +1,54 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {StyleSheet, Image, View} from 'react-native';
 
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 50,
+    },
+    pattern: {
+        width: '100%',
+        resizeMode: 'stretch',
+        height: '30%'
+    },
+    logoContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logo: {
+        width: '70%',
+        resizeMode: 'contain',
+        height: 200
+    }
+});
+  
 class WelcomeScreen extends Component {
+
+    componentDidMount() {
+        setTimeout(() => this.props.navigation.navigate('AuthNavigator'), 2000)
+    }
 
     render() {
       return (
         <>
-            <Text>Hello</Text>
+            <View style={{flexDirection: "column", flex: 1}}>
+                <Image 
+                    source={require("../assets/images/splash_top.png")}
+                    style={styles.pattern}
+                />
+                <View 
+                    style={styles.logoContainer}
+                >
+                    <Image 
+                        source={require("../assets/images/blast_logo.png")}
+                        style={styles.logo}
+                    />
+                </View>
+                <Image 
+                    source={require("../assets/images/splash_bottom.png")}
+                    style={styles.pattern}
+                />
+            </View>
         </>
       );
     }
